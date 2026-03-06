d9066a62d656f38ed008771354f016ea70f64e06
AbolfazlAdhami <aadhami880@gmail.com>
2026-03-06 05:04:07 +0330

    permutation

---
+++ Python/matrix/permutation.py
@@ -0,0 +1,14 @@
+def perm(i, n, s):
+    if i == n-1:
+        print(' '.join(s))
+    else:
+        for j in range(i, n):
+            s[i], s[j] = s[j], s[i]  # swap
+            perm(i+1, n, s)
+            s[i], s[j] = s[j], s[i]  # swap
+
+
+N = int(input())
+S = [chr(ord('1')+i) for i in range(N)]
+print(S)
+perm(0, N, S)
